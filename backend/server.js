6e8441c085be4d317d1a639e424211ef75616da5
MalcolmX2145 <ekajjamalcolm@gmail.com>
2024-11-24 16:59:44 +0300

    Last commit for production code

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -9,9 +10,19 @@ const app = express(); // variable that holds the express methods
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // middleware which allows us to accept JSON data into the req.body
 
 app.use("/api/products", productRoutes); // this will prefix all the routes in product.route.js with /api/products
+
+// code for deployment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 //checks(listens) on port 5000 for any activity
 app.listen(PORT, () => {
